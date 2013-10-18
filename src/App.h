7df0891e1b02a1a41c67165123b9a49655f2e327
Kuba Sławiński <kanngaxx@gmail.com>
2013-10-18 09:58:46 +0200

    Removed unnecessary comments

--- src/App.h
+++ src/App.h
@@ -2,8 +2,6 @@
 // Created by Kuba Sławiński on 11/10/2013.
 // Copyright (c) 2013 Kuba Sławiński. All rights reserved.
 //
-// To change the template use AppCode | Preferences | File Templates.
-//
 
 
 
